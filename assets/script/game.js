a197c715d4eb11c3018631db0bf4735b9de60de9
Guilherme Rosa <guilhermebr84@gmail.com>
2022-02-16 21:12:55 -0300

    lets do it

---
+++ assets/script/game.js
@@ -0,0 +1,8 @@
+// all the game rules will be insert here
+
+// roll scenarios, between urban and countryside (rural)
+// insert randoms energy consumptions (kWh.month) for each building and shows it, 
+// insert pv modules - insert more than one and it decrease the energy consumptions.
+// more of panels you insert, more the energy consumptions decrease and more energy is sent to the energy grid (red for consumptio, green for energy to grid)
+
+
---
+++ assets/script/index.js
@@ -0,0 +1,4 @@
+// all the games objects will be insert here
+
+
+// objects: sun, sunbeams, building, energy consumption, pv modules
