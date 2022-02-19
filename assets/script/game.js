307fec2abdf42959bc6cb00a39e17ba0d6fdf0ed
Guilherme Rosa <guilhermebr84@gmail.com>
2022-02-19 10:01:43 -0300

    19-02-2022 10:01

--- assets/script/game.js
+++ assets/script/game.js
@@ -5,26 +5,35 @@
 // inserir os modulos fv e a cada um que é inserido o consumo de energia reduz
 // quanto mais modulo inserir, o valor de consumo reduz. Se negativar, aparecer uma mensagem "energia sendo injetada na rede" ou algo parecido
 
+
+
 console.log('game.js tá rodando')
 
 class Game {
-    constructor(){
 
+    // objetos - jogador, telhado e modulo PV
+    constructor(player, roof, pvMod){
+        this.player = player;
+        this.roof = roof;
+        this.pvMod = pvMod;
+    }
+    
+    checkPlayerSaves() {
+    if (this.player.energysave > 1000){
+        return `you save ${this.energysave} kwh.month`;
+    } 
     }
 }
-function buildingsConsumption (value) {
 
+class Player {
+    constructor(name){
+        this.name = name;
+        this.win = ''
+    }
+
+    // insere o modulo PV sobre os locais indicados
+    setPV(insertPv){
+        this.insertPV = insertPV;
+        this.energySave = 0
+    }
 }
-
-function insertPV (value) {
-
-}
-
-function deletePV (value) {
-
-}
-
-function decreaseConsumptions (value) {
-
-}
-
--- assets/script/index.js
+++ assets/script/index.js
@@ -3,6 +3,52 @@
 console.log('index.js tá rodando')
 
 
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+
+console.log(btn1);
+
+function handleInsertBtn(){
+
+}
+
+// todos os botões do jogo, para cada área
+btn1.addEventListener('click', () => {
+    console.log("botão 1")
+    handleInsertBtn()
+})
+
+btn2.addEventListener('click', () => {
+    console.log("botão 2")
+    handleInsertBtn()
+})
+
+btn3.addEventListener('click', () => {
+    console.log("botão 3")
+    handleInsertBtn()
+})
+
+btn4.addEventListener('click', () => {
+    console.log("botão 4")
+    handleInsertBtn()
+})
+
+btn5.addEventListener('click', () => {
+    console.log("botão 5")
+    handleInsertBtn()
+})
+
+btn6.addEventListener('click', () => {
+    console.log("botão 6")
+    handleInsertBtn()
+})
+
+
+
 class PVinsertion {
 
     constructor(xPos, yPos) {
